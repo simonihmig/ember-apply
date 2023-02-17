755d253ecdacfa3688f296f54e5f82295065b6bb
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-02-17 10:22:40 -0500

    Refactor, things already do what I want

--- ember-apply/src/-private/project/index.js
+++ ember-apply/src/-private/project/index.js
@@ -3,14 +3,8 @@
 /**
  * Public API for the project namespace
  */
+export { gitRoot, gitIgnore } from './git.js';
 
-export {
-  gitRoot,
-  gitIgnore,
-  getPackageManager,
-  getPackageManagerLockfile,
-  inWorkspace,
-  eachWorkspace,
-  getWorkspaces,
-  workspaceRoot,
-} from './utils.js';
+export { getPackageManager, getPackageManagerLockfile } from './package-manager.js';
+
+export { inWorkspace, eachWorkspace, getWorkspaces, workspaceRoot } from './workspace.js';
